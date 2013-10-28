444d69f73cddf039b5aaea1988a8ca9abef982e5
Dmitry Vyazelenko <vyazelenko@yahoo.com>
2013-10-28 21:13:42 +0100

    Run script updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 JAVA_EXEC=$1/bin/java
 
-$JAVA_EXEC -Xms3600m -Xmx4000m -jar target/microbenchmarks.jar -gc true -foe true -f 5
+$JAVA_EXEC -showversion -server -Xms3600m -Xmx4000m -jar target/microbenchmarks.jar -gc true -foe true -f 5
